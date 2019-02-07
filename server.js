99e5c6e08b2f82fe06bce0dff65dc0676cc8e508
jjmonte20 <jjmonte20@gmail.com>
2019-02-07 12:42:27 -0600

    able to see nicknames when sending messages

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ io.sockets.on('connection', function(socket){
     });
 
     socket.on('send message', function(data){
-        io.sockets.emit('new message', data);
+        io.sockets.emit('new message', { msg: data, nick: socket.nickname });
     });
 
     function updateNicknames(){
