861da1af3c445140d18ee9ae90ae3fffa341fdff
jjmonte20 <jjmonte20@gmail.com>
2019-02-07 12:27:48 -0600

    able to login as individual users

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var nicknames = [];
 
 var PORT = process.env.PORT || 3000;
 
@@ -12,6 +13,18 @@ app.get("/", function(req, res) {
 });
 
 io.sockets.on('connection', function(socket){
+
+    socket.on('new user', function(data, callback){
+        if (nicknames.indexOf(data) != -1){
+            callback(false);
+        } else {
+            callback(true);
+            socket.nickname = data;
+            nicknames.push(socket.nickname);
+            io.sockets.emit('usernames', nicknames);
+        }
+    });
+
     socket.on('send message', function(data){
         io.sockets.emit('new message', data);
     });
