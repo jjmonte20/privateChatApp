25d56abe9a15f9ff51666553684a531b90e29473
jjmonte20 <jjmonte20@gmail.com>
2019-02-07 14:13:59 -0600

    added notes for future reference

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+// Boilerplate text for socket.io to work with mongoose
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var mongoose = require('mongoose');
+// a list of users
 var users = {};
 
 var PORT = process.env.PORT || 3000;
@@ -17,6 +19,7 @@ mongoose.connect('mongodb://localhost/chat', function(err){
     }
 });
 
+// chat schema
 var chatSchema = mongoose.Schema({
     nick: String,
     msg: String,
@@ -25,16 +28,22 @@ var chatSchema = mongoose.Schema({
 
 var Chat = mongoose.model('Message', chatSchema);
 
+// get route for the homepage
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+// whenever socket connects, send this function
 io.sockets.on('connection', function(socket){
+    // shorthand for Chat.find
     var query = Chat.find({});
+    // in this case, we're sending a Chat.find for all messages, limiting to the 8 most recent, and sorting in reverse
     query.sort('-created').limit(8).exec(function(err, docs) {
         if (err) throw err;
+        // emitting to the front end old messages, passing in the data from the database
         socket.emit('load old msgs', docs);
     });
+    // whenever we make a new user, make a new function that has the data send, and a callback of true or falses
     socket.on('new user', function(data, callback){
         if (data in users){
             callback(false);
@@ -46,14 +55,18 @@ io.sockets.on('connection', function(socket){
         }
     });
 
+    // on send message run this function with data and a callback function
     socket.on('send message', function(data, callback){
         var msg = data.trim();
+        // check if this is intended to be public or private
         if (msg.substr(0,3) === "/w ") {
             msg = msg.substr(3);
             var ind = msg.indexOf(' ');
+            // checking that there is a message to whisper
             if(ind !== -1) {
                 var name = msg.substring(0, ind);
                 var msg = msg.substring(ind + 1);
+                // checking that the user is whispering to another user
                 if (name in users) {
                     users[name].emit('whisper', { msg: msg, nick: socket.nickname })
                     // console.log('whisper');
@@ -64,6 +77,7 @@ io.sockets.on('connection', function(socket){
                 callback('Err: Please enter a message for you whisper');
             }
         } else {
+            // if not a whisper, send to everyone
             var newMsg = new Chat({msg: msg, nick: socket.nickname});
             newMsg.save(function(err){
                 if (err) throw err;
@@ -77,10 +91,13 @@ io.sockets.on('connection', function(socket){
         io.sockets.emit('usernames', Object.keys(users));
     }
 
+    // on disconnect, check if the user input a username or not
     socket.on('disconnect', function(data){
+        // if they did not, don't worry about it, there isn't much needed to fix
         if (!socket.nickname) {
             return;
         } else {
+            // if they did, delete the specific user this person was, they're not needed, then update the username
             delete users[socket.nickname];
             updateNicknames();
         }
