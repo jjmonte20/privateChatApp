b1675199385711b74c7252d925cdf3c93f6895c8
jjmonte20 <jjmonte20@gmail.com>
2019-02-07 13:01:18 -0600

    initial code for private messaging

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var nicknames = [];
+var users = {};
 
 var PORT = process.env.PORT || 3000;
 
@@ -15,29 +15,47 @@ app.get("/", function(req, res) {
 io.sockets.on('connection', function(socket){
 
     socket.on('new user', function(data, callback){
-        if (nicknames.indexOf(data) != -1){
+        if (data in users){
             callback(false);
         } else {
             callback(true);
             socket.nickname = data;
-            nicknames.push(socket.nickname);
+            users[socket.nickname] = socket;
             updateNicknames();
         }
     });
 
-    socket.on('send message', function(data){
-        io.sockets.emit('new message', { msg: data, nick: socket.nickname });
+    socket.on('send message', function(data, callback){
+        var msg = data.trim();
+        if (msg.substr(0,3) === "/w ") {
+            msg = msg.substr(3);
+            var ind = msg.indexOf(' ');
+            if(ind !== -1) {
+                var name = msg.substring(0, ind);
+                var msg = msg.substring(ind + 1);
+                if (name in users) {
+                    users[name].emit('whisper', { msg: msg, nick: socket.nickname })
+                    console.log('whisper');
+                } else {
+                    callback('error: enter a valid user');
+                }
+            } else {
+                callback('Err: Please enter a message for you whisper');
+            }
+        } else {
+        io.sockets.emit('new message', { msg: msg, nick: socket.nickname });
+        }
     });
 
     function updateNicknames(){
-        io.sockets.emit('usernames', nicknames);
+        io.sockets.emit('usernames', Object.keys(users));
     }
 
     socket.on('disconnect', function(data){
         if (!socket.nickname) {
             return;
         } else {
-            nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+            delete users[socket.nickname];
             updateNicknames();
         }
     });
