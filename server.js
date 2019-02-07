2e4d8d7e49500238dbe63fa0601d5f09ddc23411
jjmonte20 <jjmonte20@gmail.com>
2019-02-07 13:52:25 -0600

    added a database and able to display the 8 most recent messages

--- server.js
+++ server.js
@@ -2,21 +2,39 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-
+var mongoose = require('mongoose');
 var users = {};
 
 var PORT = process.env.PORT || 3000;
 
 server.listen(PORT);
 
+mongoose.connect('mongodb://localhost/chat', function(err){
+    if (err){
+        console.log(err);
+    } else {
+        console.log("Connected to mongoDB");
+    }
+});
 
+var chatSchema = mongoose.Schema({
+    nick: String,
+    msg: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Chat = mongoose.model('Message', chatSchema);
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 io.sockets.on('connection', function(socket){
-
+    var query = Chat.find({});
+    query.sort('-created').limit(8).exec(function(err, docs) {
+        if (err) throw err;
+        socket.emit('load old msgs', docs);
+    });
     socket.on('new user', function(data, callback){
         if (data in users){
             callback(false);
@@ -46,9 +64,14 @@ io.sockets.on('connection', function(socket){
                 callback('Err: Please enter a message for you whisper');
             }
         } else {
-        io.sockets.emit('new message', { msg: msg, nick: socket.nickname });
+            var newMsg = new Chat({msg: msg, nick: socket.nickname});
+            newMsg.save(function(err){
+                if (err) throw err;
+                io.sockets.emit('new message', { msg: msg, nick: socket.nickname });
+            });
         }
     });
+
 
     function updateNicknames(){
         io.sockets.emit('usernames', Object.keys(users));
